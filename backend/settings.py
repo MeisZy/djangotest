3f16069ad6f858a6f7fa63288ccc7876183bc033
MeisZy <zyrusnw@gmail.com>
2026-02-23 13:17:25 +0800

    fix: deleted SECRET_KEY from settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4k-rn&i)jq4p8mt^fwx7%&moi!)@k*de36jo)$f#b#*-0p212l"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
